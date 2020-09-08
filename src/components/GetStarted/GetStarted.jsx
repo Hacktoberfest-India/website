2de6d5ff62ffc70aa39168c90836770b0c307610
r-ush <aarush.bhatt@gmail.com>
2020-09-08 13:16:28 +0530

    added get started page

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 import Step from "./Step";
+import classes from "./getStarted.module.css";
 
 function GetStarted() {
     return (
-        <div>
-            <h2>Get Started</h2>
-            <Step />
-            <Step />
+        <div className={classes.getStarted}>
+            <Typography className={classes.title} variant="h2">
+                Get Started
+            </Typography>
+            <Step
+                number="1"
+                text="Banner blah blah Main Banner blah blahMain Banner blah blah"
+            />
+            <Step
+                number="2"
+                text="Banner blah blah Main Banner blah blahMain Banner blah blah"
+            />
+            <Step
+                number="3"
+                text="Banner blah blah Main Banner blah blahMain Banner blah blah"
+            />
+            <Step
+                number="4"
+                text="Banner blah blah Main Banner blah blahMain Banner blah blah"
+            />
         </div>
     );
 }
