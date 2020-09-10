ee670d279f476e916968be1dc4f6314189bd536d
SimranMakhija7 <simran.makhija2@gmail.com>
2020-09-10 21:51:58 +0530

    Added sponsors

--- src/components/Sponsors/Sponsor.jsx
+++ src/components/Sponsors/Sponsor.jsx
@@ -1,6 +1,42 @@
 import React from "react";
-// props include logo and category
-function Sponsor() {
-    return <div>sponsor</div>;
+import { Grid, Card, CardMedia} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+// props include logo
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    cardMedia: {
+      paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
+
+
+// eslint-disable-next-line react/prop-types
+const Sponsor = ({ logo, size }) => {
+    const classes = useStyles();
+
+    return (
+              <Grid item xs={size} >
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={logo}
+                    title="Image title"
+                  />
+                </Card>
+              </Grid>
+    )
 }
 export default Sponsor;
--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import { Container, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles"
 import Sponsor from "./Sponsor";
+import Classes from "../Timeline/timeline.module.css";
 
-function Sponsors() {
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+}));
+
+function Team() {
+    const classes = useStyles();
+
     return (
         <div>
-            <h2>Sponsors</h2>
-            <Sponsor />
-            <Sponsor />
+            <Typography className={Classes.title} variant="h2">
+                Sponsors
+            </Typography> 
+            <Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <Typography variant="h3">
+                            Platinum Sponsors
+                        </Typography>  
+                    </Grid>
+                    <Sponsor logo="https://source.unsplash.com/random" size="12" />
+                    <Grid item xs={12}>
+                        <Typography variant="h3">
+                            Gold Sponsors
+                        </Typography>  
+                    </Grid>
+                    <Sponsor logo="https://source.unsplash.com/random" size="6" />
+                    <Sponsor logo="https://source.unsplash.com/random" size="6" />
+                    <Grid item xs={12}>
+                        <Typography variant="h3">
+                            Silver Sponsors
+                        </Typography>  
+                    </Grid>
+                    <Sponsor logo="https://source.unsplash.com/random" size="4" />
+                    <Sponsor logo="https://source.unsplash.com/random" size="4" />
+                    <Sponsor logo="https://source.unsplash.com/random" size="4" />
+                </Grid>
+            </Container>
         </div>
     );
 }
-export default Sponsors;
+export default Team;
